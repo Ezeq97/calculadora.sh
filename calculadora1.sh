52f202b1741339e8d0927d13df3cf82d2d4e36bb
rrayen97 <97424869+rrayen97@users.noreply.github.com>
2022-12-02 15:25:25 -0300

    Add files via upload

---
+++ calculadora1.sh
@@ -0,0 +1,56 @@
+#!/bin/sh 
+ echo "Elige la operaciòn a relizar" 
+ while :  
+ do  
+ read INPUT_STRING  
+  case $INPUT_STRING  in  
+  suma)      echo  "Haz elegido suma" 
+           echo " Ingresa tu primer numero" 
+           read n1 
+           echo  "Ingresa tu siguiente numero " 
+           read n2  
+ resultado=$(($n1 + $n2)) 
+  
+ echo "Tu resultado es: $resultado" 
+          ;; 
+  
+  resta)  echo  "Haz elegido resta" 
+ echo "Ingresa tu primer numero" 
+ read n1  
+  echo "Ingresa tu siguiente nùmero" 
+ read n2  
+  
+ resultado_resta=$(($n1 -$n2)) 
+ echo "Tu resultado de la resta es:  $resultado_resta"                 
+  
+ ;;   
+  
+ multiplicar)  echo    "Haz elegido multiplicar" 
+ echo  "Ingresa tu primer nùmero:" 
+ read n1  
+ echo "ingresa el siguiente numero:" 
+ read n2 
+  
+ resultado_multiplicar=$(($n1 * $n2)) 
+  
+ echo "Tu resultado es: $resultado_multiplicar" 
+  
+                   ;; 
+ dividir) echo "Haz elegido dividir " 
+ echo  "Ingresa tu primer nùmero:" 
+ read n1  
+ echo "Ingresa el siguiente nùmero:" 
+ read n2  
+  
+ resultado_dividir=$(($n1 / $n2)) 
+  
+ echo "Tu resultado es:  $resultado_dividir" 
+ ;; 
+  
+  
+  
+ *)    echo "No haz elegido ninguna operaciòn Bye!" 
+           break  
+          ;; 
+ esac 
+ done
